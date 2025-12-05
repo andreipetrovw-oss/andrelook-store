b36ddefcdd7b3092790d7d6c429bf4f0dd5d88f7
Mac <mac@Macs-MacBook-Pro.local>
2025-12-06 01:11:50 +0200

    Disable seed.ts in build

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,1 @@
-import { PrismaClient } from '@prisma/client/runtime/library';
-;
-import bcrypt from 'bcryptjs';
-
-const prisma = new PrismaClient();
-
-async function main() {
-  // твой код сидирования (оставь как есть)
-}
-
-main()
-  .then(() => prisma.$disconnect())
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+export {};
